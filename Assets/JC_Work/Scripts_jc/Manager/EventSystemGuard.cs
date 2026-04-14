27849be6c9efcce54e46476e98c04a216eb70ac8
rock-oon <jcgs0080@gmail.com>
2026-04-14 12:55:19 +0900

    [fix] 타 팀원 씬에 EventSystem 오브젝트 존재 시, GameManager의 것과 중복되는 현상 해결

--- Assets/JC_Work/Scripts_jc/Manager/EventSystemGuard.cs
+++ Assets/JC_Work/Scripts_jc/Manager/EventSystemGuard.cs
@@ -1,34 +1,55 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(EventSystem))]
 public class EventSystemGuard : MonoBehaviour
 {
+    private EventSystem self;
+
     private void Awake()
     {
-        // 씬에 이미 다른 EventSystem이 있으면 자신을 비활성화
-        var allEventSystems = FindObjectsOfType<EventSystem>();
-        foreach (var es in allEventSystems)
-        {
-            if (es != GetComponent<EventSystem>() && es.gameObject != gameObject)
-            {
-                gameObject.SetActive(false);
-                return;
-            }
-        }
+        self = GetComponent<EventSystem>();
     }
 
     private void OnEnable()
     {
-        // 씬 전환 후 다시 활성화 시 체크
-        var allEventSystems = FindObjectsOfType<EventSystem>();
-        int activeCount = 0;
-        foreach (var es in allEventSystems)
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+        // 초기 Recheck는 하지 않는다.
+        // BeforeSceneLoad 시점에 다른 씬의 EventSystem이 아직 생성되지 않아 판정 불가.
+        // AfterSceneLoad 훅과 sceneLoaded 이벤트가 이후 enable 여부를 결정한다.
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
+
+    private void OnSceneLoaded(Scene s, LoadSceneMode m) => Recheck();
+    private void OnSceneUnloaded(Scene s) => Recheck();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void InitialRecheck()
+    {
+        foreach (EventSystemGuard guard in FindObjectsOfType<EventSystemGuard>())
+            guard.Recheck();
+    }
+
+    private void Recheck()
+    {
+        if (self == null) self = GetComponent<EventSystem>();
+        if (self == null) return;
+
+        EventSystem[] all = FindObjectsOfType<EventSystem>();
+        bool duplicateActive = false;
+        foreach (EventSystem other in all)
         {
-            if (es.gameObject.activeInHierarchy) activeCount++;
+            if (other == self) continue;
+            if (other.gameObject.activeInHierarchy && other.enabled) { duplicateActive = true; break; }
         }
-        if (activeCount > 1)
-        {
-            gameObject.SetActive(false);
-        }
+
+        self.enabled = !duplicateActive;
     }
 }
